6742ea11c0820a7d751fb3e028735ef75a51a8e0
ihnat13 <ignatfomich7@gmail.com>
2024-01-18 22:15:21 +0200

    hw15

---
+++ HW15.js
@@ -0,0 +1,47 @@
+const userInput = document.getElementById('name-input-modal');
+const statsModal = document.getElementById('welcome-modal');
+const modalInput = document.querySelector(`.modal-content`);
+const modalShowStats = document.getElementById(`modal-content2`);
+showUserInputValue();
+function showUserInputValue() {
+userInput.style.display = 'block';
+document.body.classList.add('modal-open');
+}
+
+modalInput.addEventListener(`click`, ({ target }) => {
+    if(target.tagName === `BUTTON`){
+        saveUserInput();
+    }
+})
+
+function saveUserInput() {
+    const inputElement = document.getElementById('name-input');
+    const enteredValue = inputElement.value.trim();
+    if (enteredValue === '') {
+        alert('Введите хотя бы одно слово.');
+        return;
+    }
+
+    const wordsArray = enteredValue.split(' ').filter(Boolean);
+    const res = wordsArray.reduce((prevGroup, el) => {
+        if (!prevGroup.has(el)) {
+            prevGroup.set(el, []);
+        }
+        prevGroup.get(el).push(el);
+        return prevGroup;
+    }, new Map());
+
+    showUniqueWords(res);
+}
+
+function showUniqueWords(username) { 
+    statsModal.style.display = 'block';
+    userInput.style.display = 'none';
+    document.body.classList.remove('modal-open');
+
+    const res2 = username.forEach((value, key) => {
+        const createEl = document.createElement(`p`)
+        createEl.innerHTML = `Слово: ${key}, Количество: ${value.length}`;
+        modalShowStats.append(createEl)
+    });
+}
